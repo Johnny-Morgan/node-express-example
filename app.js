7ae3bef5e1f8c6e7a858f6a4990c7bf8e6112637
John Morgan <jwjmorgan@gmail.com>
2021-12-03 21:02:37 +0000

    Add route to display users

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 const express = require("express");
+const { response } = require("express");
 
 const app = express();
 
@@ -30,6 +31,20 @@ app.post("/store-user", function (req, res) {
   fs.writeFileSync(filePath, JSON.stringify(existingUsers));
 
   res.send("<h1>Username stored!</h1>");
+});
+
+app.get("/users", function (req, res) {
+  const filePath = path.join(__dirname, "data", "users.json");
+
+  const fileData = fs.readFileSync(filePath);
+  const existingUsers = JSON.parse(fileData);
+
+  let responseData = "<ul>";
+  for (const user of existingUsers) {
+    responseData += `<li>${user}</li>`;
+  }
+  responseData += "</ul>";
+  res.send(responseData);
 });
 
 app.listen(3000);
