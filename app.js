c0ee9edcf62019fa38141e04c3c073e1f0dd149e
John Morgan <jwjmorgan@gmail.com>
2021-12-03 11:59:43 +0000

    Add post method route

--- app.js
+++ app.js
@@ -8,8 +8,14 @@ app.get("/current-time", function (req, res) {
 
 app.get("/", function (req, res) {
   res.send(
-    '<form action="/store-user" method="POST"><label>Your Name </label><input type="text"><button>Submit</button></form>'
+    '<form action="/store-user" method="POST"><label>Your Name </label><input type="text" name="username"><button>Submit</button></form>'
   );
+});
+
+app.post("/store-user", function (req, res) {
+  const username = req.body.username;
+  console.log(username);
+  res.send("<h1>Username stored!</h1>");
 });
 
 app.listen(3000);
